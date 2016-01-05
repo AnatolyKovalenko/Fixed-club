82e04a4443a17f94c395df70a760e7120519be68
AnatolyKovalenko <mr.anatolykovalenko@gmail.com>
2016-01-05 11:02:00 +0200

    Add rotate effect to footer wheel

--- js/common.js
+++ js/common.js
@@ -6,7 +6,27 @@ $(function() {
 	navBut.on('click', function(){
 		navMenu.slideToggle();
 	});
-  	
+  
+  //Footer wheel
+	$('#bic').on({
+   	mouseenter: function() {
+		  $(this).css({
+				'-webkit-transform':'rotate(720deg)',
+		    '-moz-transform':'rotate(720deg)',
+		   	'-o-transform':'rotate(720deg)',
+		   	'transform':'rotate(720deg)'
+	    });
+  	},
+	  mouseleave: function() {
+		  $(this).css({
+				'-webkit-transform':'rotate(0deg)',
+		   	'-moz-transform':'rotate(0deg)',
+		 		'-o-transform':'rotate(0deg)',
+		   	'transform':'rotate(0deg)'
+		 });
+  	}  
+	});
+		
 });
 	
 	
