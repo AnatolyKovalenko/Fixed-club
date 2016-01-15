b802cac4a55c4d6f1f431ad99b88f74639e164bd
AnatolyKovalenko <mr.anatolykovalenko@gmail.com>
2016-01-15 19:25:43 +0200

    Create delivery and payment page

---
+++ page-delivery-and-payment-page.php
@@ -0,0 +1,103 @@
+
+<?php get_header(); ?>
+	
+	<!--Section grey-string-->
+	<section class="grey-string">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2> 
+						Доставка
+					</h2>
+				</div>
+			</div>
+		</div>	
+	</section>
+
+	<!--Section -->
+	<section class="delivery">
+		<div class="container">
+			<div class="container-back">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="content-box">
+							<div class="container-text left">
+								<h2>
+									Варианты доставки транспортной компанией "Нова Пошта" по Украине:
+									<br>
+									• Доставка на отделение — Бесплатно.
+									<br>
+									• Доставка курьером по указанному адресу — 30-50 грн.
+								</h2>
+								<p>
+									Отправка осуществляется в течение 1-2 дней после оформления заказа.
+									Сроки доставки составляют 2-3 дня с момента отправки.
+									<br>
+									В Ровно возможен вариант самовывоза.
+									По предварительной договорённости с менеджером, заказ будет ожидать Вас на складе.
+									<br>
+									<br>
+									Обратите внимание, что велосипеды отправляются в заводской упаковке.
+									при этом поставляется велосипед собран на 80% (в установке нуждаются: переднее колесо, руль, седло, педали).
+									Для сборки Вам понадобятся: гаечный ключ на 15, шестигранники на 4, 5, 6 мм, насос.
+									<br>
+									<br>
+									Перед отправкой все товары проверяются на отсутствие дефектов и комплектацию.
+								</p>
+							</div>
+							<div class="container-bg up">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--Section grey-string-->
+	<section class="grey-string">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2> 
+						Оплата
+					</h2>
+				</div>
+			</div>
+		</div>	
+	</section>
+
+	<!--Section about-down-->
+	<section class="payment">
+		<div class="container">
+			<div class="container-back">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="content-box">
+							<div class="container-bg down">
+							</div>
+							<div class="container-text right">
+								<h2>
+									Наличными:
+								</h2>
+								<p>
+									• При получении товара — Наложенный Платеж (Новой Поштой взимается комиссия за перевод денег — 2% от суммы заказа)
+									<br>
+									• При самовывозе в Ровно.
+								</p>
+								<h2>
+									Электронные платежи:
+								</h2>
+								<p>
+									Благодаря обширной региональной сети банков "ПриватБанк", 
+									Вы можете осуществлять оперативные переводы наличных денежных средств.  
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php get_footer();?>
